c2abaeb06c108247190fcb5498ead2a473699ba5
Frank Bernal <frankbernal@mail.fresnostate.edu>
2025-12-01 15:26:40 -0800

    uibuild

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const Note = require('./models/note');
 
 const app = express();
-
-app.use(express.json());
-app.use(express.static('dist'));
 
 let notes = [
     {
@@ -26,13 +22,6 @@ let notes = [
     }
 ];
 
-const generateID = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => Number(n.id)))
-        : 0;
-    return String(maxId + 1);
-}
-
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method);
     console.log('Path:  ', request.path);
@@ -42,6 +31,8 @@ const requestLogger = (request, response, next) => {
 }
 
 app.use(requestLogger);
+app.use(express.static('dist'));
+app.use(express.json());
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>');
@@ -57,13 +48,6 @@ app.get('/api/notes/:id', (request, response) => {
     Note.findById(request.params.id).then(note => {
         response.json(note);
     });
-});
-
-app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id;
-    notes = notes.filter(note => note.id !== id);
-
-    response.status(204).end();
 });
 
 app.post('/api/notes', (request, response) => {
@@ -85,9 +69,17 @@ app.post('/api/notes', (request, response) => {
     });
 });
 
+app.delete('/api/notes/:id', (request, response) => {
+    const id = request.params.id;
+    notes = notes.filter(note => note.id !== id);
+
+    response.status(204).end();
+});
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' });
 };
+
 app.use(unknownEndpoint);
 
 const PORT = process.env.PORT;
