2689a8fcc0f59b28ca163e93260e4a9121a27532
Frank Bernal <frankbernal@mail.fresnostate.edu>
2025-12-02 12:17:44 -0800

    uibuild

--- models/note.js
+++ models/note.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log('connecting to', url);
+console.log('connecting to', url)
 
 mongoose.connect(url, { family: 4 })
-    .then(result => {
-        console.log('connected to MongoDB');
+    .then( () => {
+        console.log('connected to MongoDB')
     })
     .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message);
-    });
+        console.log('error connecting to MongoDB:', error.message)
+    })
 
 const noteSchema = new mongoose.Schema({
     content: {
@@ -21,14 +21,14 @@ const noteSchema = new mongoose.Schema({
         required: true,
     },
     important: Boolean,
-});
+})
 
 noteSchema.set('toJSON', {
     transform: (document, retrunedObject) => {
-        retrunedObject.id = retrunedObject._id.toString();
-        delete retrunedObject._id;
-        delete retrunedObject.__v;
+        retrunedObject.id = retrunedObject._id.toString()
+        delete retrunedObject._id
+        delete retrunedObject.__v
     }
-});
+})
 
-module.exports = mongoose.model('Note', noteSchema);
+module.exports = mongoose.model('Note', noteSchema)
