a771d384e132edf10c01f52cda888a98994a8975
Frank Bernal <frankbernal@mail.fresnostate.edu>
2025-12-03 10:03:03 -0800

    Separated areas of concern. Everything is modular now.

--- models/note.js
+++ models/note.js
@@ -1,20 +1,4 @@
-const logger = require('../utils/logger')
-const config = require('../utils/config')
 const mongoose = require('mongoose')
-
-mongoose.set('strictQuery', false)
-
-const url = config.MONGODB_URI
-
-logger.info('connecting to', url)
-
-mongoose.connect(url, { family: 4 })
-    .then( () => {
-        logger.info('connected to MongoDB')
-    })
-    .catch((error) => {
-        logger.error('error connecting to MongoDB:', error.message)
-    })
 
 const noteSchema = new mongoose.Schema({
     content: {
